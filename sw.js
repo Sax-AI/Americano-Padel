2a0f6cda8b0e49171cb2781842d8274dd5c6c488
Sax-AI <saxespark@gmail.com>
2026-01-13 11:02:30 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "americano-padel-v8";
+const CACHE_NAME = "americano-padel-v9";
 const FILES = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", e => {
